ba2911669f9d9b9ef035d053f2bfcc2b1d500a5c
Behrooz Shahin <shahin.behrooz@gmail.com>
2024-12-24 14:59:11 -0500

    initial commit

---
+++ src/main/java/com/hydro/suply/SuplyApplication.java
@@ -0,0 +1,13 @@
+package com.hydro.suply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hydro/suply/SuplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydro.suply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
